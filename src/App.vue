0d97bb3d12bfafd9967da4e2b48263e790f6b499
initialencounter <2911583893@qq.com>
2024-02-17 22:01:39 +0800

    feat: adjust css

--- src/App.vue
+++ src/App.vue
@@ -103,7 +103,7 @@ function handleClearList() {
             d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
       </svg>
     </a>
-    <p> 文件 MD5 校对器 </p>
+    <p style="font-size: 24px"> 文件 MD5 校对器 </p>
     <br>
     <div class="header">
       <div class="cell-name">名称</div>
@@ -174,7 +174,7 @@ function handleClearList() {
   position: absolute;
   bottom: 4%;
   right: 4%;
-  font-size: 12px;
+  font-size: 14px;
   padding: 0 5px;
 }
 
@@ -189,7 +189,7 @@ function handleClearList() {
   padding: 10px;
   width: 100%;
   height: 30px;
-  font-size: 14px;
+  font-size: 16px;
   font-weight: bold;
   display: flex;
   align-items: center;
@@ -203,6 +203,7 @@ function handleClearList() {
 
 .cell-name {
   float: left;
+  padding: 2px;
   width: 100%;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -231,11 +232,10 @@ function handleClearList() {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 10px;
   width: 100%;
-  height: 30px;
+  height: 25px;
   color: #77ce62;
-  font-size: 12px;
+  font-size: 14px;
   margin: 2px 0;
   border-radius: 4px;
 }
