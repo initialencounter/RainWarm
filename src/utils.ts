9e727155d3a559c0b43e39ceba401063c6291f1c
initialencounter <2911583893@qq.com>
2024-02-17 18:28:17 +0800

    chore: code format

--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,6 @@ export function formatTimestamp(timestamp: number): string {
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const seconds = date.getSeconds().toString().padStart(2, '0');
-    const millisecond = date.getMilliseconds().toString().padStart(3,'0')
+    const millisecond = date.getMilliseconds().toString().padStart(3, '0')
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}:${millisecond}`;
 }
