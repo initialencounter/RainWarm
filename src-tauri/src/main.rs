766ade02d6405bdedcece46571b4c999196cf2d3
initialencounter <2911583893@qq.com>
2024-02-18 17:16:06 +0800

    feat: build release by workflow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use md5::compute;
+// use md5::compute;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn drag_file(data: String) -> String {
-    let result = compute(data.as_bytes());
-    let md5_hex: String = format!("{:x}", result);
-    md5_hex[..16].to_string()
-}
+// #[tauri::command]
+// fn drag_file(data: String) -> String {
+//     let result = compute(data.as_bytes());
+//     let md5_hex: String = format!("{:x}", result);
+//     md5_hex[..16].to_string()
+// }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![drag_file])
+        // .invoke_handler(tauri::generate_handler![drag_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
