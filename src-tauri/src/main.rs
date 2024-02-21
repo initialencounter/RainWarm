f2699897cea262a3e695cb4c9d5d1c9f8ac02594
initialencounter <2911583893@qq.com>
2024-02-21 15:47:39 +0800

    refa(backend): use mod

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use reqwest;
 use std::thread;
 use std::time::Duration;
-use serde::Deserialize;
 use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
-use webbrowser;
+mod:utils
+use utils:: {open_link, get_latest_version}
 
-#[derive(Deserialize)]
-struct Release {
-    tag_name: String,
-}
 fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "退出(X)");
     let hide = CustomMenuItem::new("hide".to_string(), "隐藏(H)");
@@ -76,35 +71,4 @@ fn main() {
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-fn open_link(url: &str) {
-    if let Err(e) = webbrowser::open(&url) {
-        eprintln!("Failed to open link: {}", e);
-    }
-}
-
-fn get_latest_version(current_version: &str) -> String {
-    let url = "https://api.github.com/repos/initialencounter/rainwarm/releases/latest";
-    let client = reqwest::blocking::Client::new();
-
-    let resp = match client
-        .get(url)
-        .header(reqwest::header::USER_AGENT, "rust-app")
-        .send()
-    {
-        Ok(response) => response,
-        Err(_) => return current_version.to_string(),
-    };
-
-    if !resp.status().is_success() {
-        return current_version.to_string();
-    }
-
-    let release = match resp.json::<Release>() {
-        Ok(release) => release,
-        Err(_) => return current_version.to_string(),
-    };
-
-    release.tag_name
 }
---
+++ src-tauri/src/utils.rs
@@ -0,0 +1,40 @@
+use reqwest;
+use webbrowser;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Release {
+    tag_name: String,
+}
+
+
+fn open_link(url: &str) {
+    if let Err(e) = webbrowser::open(&url) {
+        eprintln!("Failed to open link: {}", e);
+    }
+}
+
+fn get_latest_version(current_version: &str) -> String {
+    let url = "https://api.github.com/repos/initialencounter/rainwarm/releases/latest";
+    let client = reqwest::blocking::Client::new();
+
+    let resp = match client
+        .get(url)
+        .header(reqwest::header::USER_AGENT, "rust-app")
+        .send()
+    {
+        Ok(response) => response,
+        Err(_) => return current_version.to_string(),
+    };
+
+    if !resp.status().is_success() {
+        return current_version.to_string();
+    }
+
+    let release = match resp.json::<Release>() {
+        Ok(release) => release,
+        Err(_) => return current_version.to_string(),
+    };
+
+    release.tag_name
+}
