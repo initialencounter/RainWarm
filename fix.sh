daff9e46786f417e614608e2d8611eb0e0720c55
initialencounter <2911583893@qq.com>
2024-06-16 17:36:35 +0800

    fix path

---
+++ fix.sh
@@ -0,0 +1,11 @@
+path=$1
+files=$(ls $path)
+for filename in $files
+do  
+    case "$filename" in
+	*.js ) 
+    echo "<script type=\"module\" crossorigin src=\"/assets/$filename\"></script>" >> $path/index.html;;
+	*.css )
+    echo "<link rel=\"stylesheet\" crossorigin href=\"/assets/$filename\">" >> $path/index.html;;
+    esac
+done
