6d37776d394909d3e9a1787ec6da5257036f5fcb
initialencounter <2911583893@qq.com>
2024-06-21 22:18:55 +0800

    feat: mute menu

--- src/App.vue
+++ src/App.vue
@@ -85,7 +85,9 @@ function getMd5(blob: Blob, id: number) {
   };
   reader.readAsArrayBuffer(blob);
 }
-
+document.oncontextmenu = function () {
+  return false;
+}
 function handleClearList() {
   file_list.value = {}
   colorList = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#42d4f4', '#f032e6', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#000075', '#a9a9a9', '#ffffff', '#000000']
